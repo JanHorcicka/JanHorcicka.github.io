b2a701b22a031ee5715e9b3b2edf3ab54b1a0009
JanHorcicka <49922794+JanHorcicka@users.noreply.github.com>
2023-08-25 20:19:13 +0200

    change box-shadow

--- chatbot.js
+++ chatbot.js
@@ -38,7 +38,7 @@ var script = document.createElement('script');script.src = "https://code.jquery.
             border-radius: 50%;
             color: white;
             cursor: pointer;
-            box-shadow: 0px 3px 16px 0px #fd5bfb, 0 3px 1px -2px #fd5bfb, 0 1px 5px 0 #fd5bfb;
+            box-shadow: 0px 3px 16px 0px rgba(0, 0, 0, 0.6), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
             display: flex;
             justify-content: center;
             align-items: center;
